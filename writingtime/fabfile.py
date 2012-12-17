15ab213932d5f574a6f3640ecd89163bd5883b4c
John Brown <thisisjohnbrown@gmail.com>
2012-12-17 10:55:53 -0800

    add dev to fabfile

--- writingtime/fabfile.py
+++ writingtime/fabfile.py
@@ -3,10 +3,19 @@ from fabric.api import *
 def live():
 	env.hosts = ['clxvi@clxvi.webfactional.com']
 	env['dir'] = '/home/clxvi/webapps/writing_time/writingtime'
+	env['apache2'] = '/home/clxvi/webapps/writing_time/apache2/bin/'
+	env['branch'] = 'master'
+	env.password = '138plus28'
+
+def dev():
+	env.hosts = ['clxvi@clxvi.webfactional.com']
+	env['dir'] = '/home/clxvi/webapps/writing_goal_dev/writingtime'
+	env['apache2'] = '/home/clxvi/webapps/writing_goal_dev/apache2/bin/'
+	env['branch'] = 'dev'
 	env.password = '138plus28'
 
 def pull():
-	run ('cd %s; git pull origin master; /home/clxvi/webapps/writing_time/apache2/bin/restart' % env['dir'])
+	run ('cd %s; git pull origin %s; %srestart' % (env['dir'], env['branch'], env['apache2']))
 
 def migration():
 	run ('cd %s; python2.7 manage.py migrate' % env['dir'])
